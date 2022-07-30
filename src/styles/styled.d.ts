4dba52c842d2cecc9b81c544a6dfc573e14983cd
jonasdevzero <jonasdevzero@gmail.com>
2022-07-30 12:50:45 -0300

    chore: disable eslint rule for empty interface

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -4,5 +4,6 @@ import { defaultTheme } from '@/styles/theme';
 declare module 'styled-components' {
   type ThemeType = typeof defaultTheme;
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends ThemeType {}
 }
