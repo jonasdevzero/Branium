e793ce4d5a6a63665159eed48f53bc181fd38a0a
jonasdevzero <jonasdevzero@gmail.com>
2022-07-30 12:32:47 -0300

    chore: add theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,9 @@
 export const defaultTheme = {
-  dark: '#000000',
+  textColor: '#FFFFFF',
+
+  backgroundColor: '#1F1F1F',
+  backgroundColorLight: '#252525',
+
+  buttonColor: '#44CF6C',
+  buttonColorDark: '#21823D',
 };
