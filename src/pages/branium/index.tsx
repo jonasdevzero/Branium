892f95e9027bb1ce69edc3b441dbba7d3c28fead
jonasdevzero <jonasdevzero@gmail.com>
2022-08-06 23:23:07 -0300

    chore: init branium page

---
+++ src/pages/branium/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Branium: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Branium</title>
+      </Head>
+
+      <main>
+        <h1>Branium - Welcome</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Branium;
