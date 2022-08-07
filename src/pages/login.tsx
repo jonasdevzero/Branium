8cef325aeb52898c62cf1deef07f9a206b8c6023
jonasdevzero <jonasdevzero@gmail.com>
2022-08-06 23:22:49 -0300

    feat: add login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,49 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
+
+import { FiUser, FiKey, FiAlertCircle } from 'react-icons/fi';
 
 import { Header } from '@/components/Header';
-import { Container } from '@/styles/utils/layout';
+import { Container, Main, Center } from '@/styles/utils/layout';
+import { Form } from '@/styles/utils/form';
+import { useFormik } from 'formik';
+import { loginValidator } from '@/validators';
+import { useAuth } from '@/hooks/useAuth';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { hasAuthTokens } from '@/utils/authTokens';
+
+const formInitialValues = {
+  username: '',
+  password: '',
+};
 
 const Login: NextPage = () => {
+  const auth = useAuth();
+  const router = useRouter();
+
+  async function handleSubmit(data: typeof formInitialValues) {
+    auth
+      .signIn(data)
+      .then(() => router.replace('/branium'))
+      .catch((error: Error) =>
+        toast.error(error.message, { position: 'top-center' }),
+      );
+  }
+
+  const formik = useFormik({
+    initialValues: formInitialValues,
+    onSubmit: handleSubmit,
+    validationSchema: loginValidator,
+    validateOnChange: false,
+  });
+
+  useEffect(() => {
+    hasAuthTokens() ? router.replace('/branium') : null;
+  }, [router]);
+
   return (
     <Container>
       <Head>
@@ -17,9 +56,75 @@ const Login: NextPage = () => {
 
       <Header isLoginHidden />
 
-      <main>
-        <h1>SignIn</h1>
-      </main>
+      <Main>
+        <Center>
+          <Form onSubmit={formik.handleSubmit}>
+            <h1 className="form__title">SignIn</h1>
+
+            <label className="form__label" htmlFor="username">
+              {formik.errors.username && (
+                <strong className="form__error">
+                  <FiAlertCircle />
+                  {formik.errors.username}
+                </strong>
+              )}
+
+              <div
+                className={`label__inner ${formik.errors.username && 'error'}`}
+              >
+                <span className="form__icon">
+                  <FiUser />
+                </span>
+
+                <input
+                  id="username"
+                  name="username"
+                  placeholder="Username"
+                  type="text"
+                  className="form__input"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                />
+              </div>
+            </label>
+
+            <label className="form__label" htmlFor="password">
+              {formik.errors.password && (
+                <strong className="form__error">
+                  <FiAlertCircle />
+                  {formik.errors.password}
+                </strong>
+              )}
+
+              <div
+                className={`label__inner ${formik.errors.password && 'error'}`}
+              >
+                <span className="form__icon">
+                  <FiKey />
+                </span>
+
+                <input
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  className="form__input"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                />
+              </div>
+            </label>
+
+            <button type="submit" className="form__submit">
+              SignIn
+            </button>
+
+            <Link href="/subscribe">
+              <a className="form__link">SignUp</a>
+            </Link>
+          </Form>
+        </Center>
+      </Main>
     </Container>
   );
 };
